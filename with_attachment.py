dba328d0eb0fbfebda0f9d33e270ff521623799f
Rajat Bansal <65078224+rajatbansal01@users.noreply.github.com>
2020-06-11 17:13:30 +0530

    Create with_attachment.py
    this is more improved version in which the system will ask user for the subject, message and the name of file to be attached but the file must be present on the dektop.

---
+++ with_attachment.py
@@ -0,0 +1,40 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import pandas as pd
+
+email_user = 'roboticscommunity996@gmail.com'
+email_password = 'startupchamps'
+e=pd.read_excel('emails.xlsx')
+email_to_send=e['students'].values
+
+subject =input("enter the subject to send\n")
+body = input('what msg you want to send\n')
+filename=input('enter file name first put it on desktop\n')
+path='\\'+filename
+attachment  =open(r'C:\Users\HP\Desktop'+path,'rb')
+
+
+msg = MIMEMultipart()
+msg['Subject'] = subject
+
+
+msg.attach(MIMEText(body,'plain'))
+
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(email_user,email_password)
+for i in email_to_send:
+    server.sendmail(email_user,i,text)
+server.quit()
+print('email sent')
